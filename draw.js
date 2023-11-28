5e48e20fee187600ff64313650521046808c39c2
omega-beta <140123838+omega-beta@users.noreply.github.com>
2023-11-28 17:15:18 -0500

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,9 +1,13 @@
 var game1 = function(processingInstance) {
     with(processingInstance) {
+        size(1000, 500);
         var x, y;
         x = mouseX;
         y = mouseY;
         var color = function() {
+            fill(0, 0, 0);
+            rect(0, 0, 1000, 500);
+            fill(255, 255, 255);
             draw(x,y);
         };
     }
