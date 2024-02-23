ea5bed2eef947ae08967a5dd2b3627fd21e639e7
omega-beta <140123838+omega-beta@users.noreply.github.com>
2024-02-23 10:44:53 -0500

    Delete draw.js

--- draw.js
+++
@@ -1,7 +0,0 @@
-var game = function(processingInstance) {
-    with(processingInstance) {
-        size(400, 400);
-    }
-};
-var canvas = document.getElementById("canvas");
-var processingInstance = new Processing(canvas, game);
